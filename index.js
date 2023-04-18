38941bb15e4175c3c2df3e1bc0f1042501e40411
Willian de Caldas Leite <decaldaswcl@gmail.com>
2023-04-18 15:43:10 -0300

    adding routes

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+//get all products
+app.get('/product', (res, req) => {
+
+})
+//get products by ID
+app.get('/product/:id', (res, req) => {
+
+})
+//include a product
+app.post('/product', (res, req) => {
+
+})
+//uptade a product
+app.put('/product/:id', (res, req) => {
+
+})
+//delete product
+app.delete('/product/:id', (res, req) => {
+
 })
 
 app.listen(port, () => {
