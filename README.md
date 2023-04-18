29be5e43bb0d5dd6ebf5f10ae1ad28d133511e89
Willian de Caldas Leite <decaldaswcl@gmail.com>
2023-04-18 16:23:09 -0300

    adding environment file

--- README.md
+++ README.md
@@ -5,4 +5,33 @@ Basic API to query, change, delete product data using node.js with MySQL databas
 # Requeriments
 
 Node.js version 18 or above
-Express version 4 or above
+
+
+# How to install
+
+1. Clone the repository:
+```sh
+git clone https://github.com/decaldaswcl/node-basic-rest-api.git
+```
+
+2. Install dependencies :
+```sh
+cd node-basic-rest-api
+
+npm install
+```
+3. Rename environment file:
+```sh
+ren environment_exemple .env
+```
+4. Set environment variables:
+
+Open the .env file and change the fields with the necessary.
+```js
+    PORT=//put the port that the server will run the application
+```
+
+
+
+# How to run
+
--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+
+require('dotenv').config();
+
+const app = express();
+
 
 //get all products
 app.get('/product', (res, req) => {
 
-})
+});
 //get products by ID
 app.get('/product/:id', (res, req) => {
 
-})
+});
 //include a product
 app.post('/product', (res, req) => {
 
-})
+});
 //uptade a product
 app.put('/product/:id', (res, req) => {
 
-})
+});
 //delete product
 app.delete('/product/:id', (res, req) => {
 
-})
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running in port ${process.env.SERVER_PORT}`);
+});
