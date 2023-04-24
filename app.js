ea9eb927df8c950f844acbcb5e0b0ee8ae8d6ef5
Willian de Caldas Leite <decaldaswcl@gmail.com>
2023-04-24 12:19:51 -0300

    database connection

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const router = require('./src/routes/routes.js')
-const mysql = require('mysql');
+
 require('dotenv').config();
 
 const app = express();
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
-
-connection.connect();
-
 
 //Enable read json in body
 app.use(express.json());
